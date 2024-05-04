dacc201bb7d3b4856a30653a7d5fbd92387b6a66
Aniket Kumar Sinha <aniket.kumar.sinha.dev.iic@gmail.com>
2024-05-04 15:40:22 +0530

    add-wss-server-api-call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ReplaySessionService } from './replay-session.service';
 import { io } from 'socket.io-client'
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,8 @@ import { io } from 'socket.io-client'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private webSocket: WebSocket
+  private messageSubject = new Subject<any>();
   constructor(private openReplayService: ReplaySessionService) {
 
     const wsHook = this.openReplayService.tracker.trackWs('my_ws_custom_channel')
@@ -22,7 +25,15 @@ export class AppComponent {
 
     socket.on('NOTIFICATION', (data) => {
       console.log("Received Notification event: ", data)
+      console.log(wsHook)
       wsHook('NOTIFICATION', data, 'down')
     })
+
+    if (!this.webSocket || this.webSocket.readyState !== WebSocket.OPEN) {
+      console.log("initializing webSocket Connections")
+      this.webSocket = new WebSocket('wss://backend-test.core.vretail.space/PR-MH800GfdsK4CeHpGYw4goEhpv?type=Sales-Person&unique_id=SP-YGONzSz3n0yYsCVRWgOx&mode=3')
+    }
+
+    fetch('https://backend-test.core.vretail.space/visitor')
   }
 }
