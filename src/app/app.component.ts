62fa0f1638ff990167f425e4a3adc492861d5234
Aniket Kumar Sinha <aniket.kumar.sinha.dev.iic@gmail.com>
2024-05-04 13:31:56 +0530

    add-socket-listner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,20 @@ import { io } from 'socket.io-client'
 })
 export class AppComponent {
   constructor(private openReplayService: ReplaySessionService) {
+
+    const wsHook = this.openReplayService.tracker.trackWs('my_ws_custom_channel')
+
     const socket = io("https://backend-test.core.vretail.space/PR-MH800GfdsK4CeHpGYw4goEhpv?type=Sales-Person&unique_id=SP-YGONzSz3n0yYsCVRWgOx&mode=3", {
       transports: ['websocket']
     })
 
     socket.on('connect', () => {
       console.log("socket Connected")
+    })
+
+    socket.on('NOTIFICATION', (data) => {
+      console.log("Received Notification event: ", data)
+      wsHook('NOTIFICATION', data, 'down')
     })
   }
 }
